f3fb4d8e809a0f689deb6a0e1962013abcd834be
Valentin De Pascale <valentindp9@gmail.com>
2025-09-27 13:56:47 -0300

    .

---
+++ exercises/tp5/03/color.py
@@ -0,0 +1,10 @@
+class Color:
+    #atributos de instancia
+    def __init__(self, rojo:int = "255", azul:int = "255", verde:int = "255"):
+        self.__rojo = rojo
+        self.__azul = azul
+        self.__verde = verde
+
+        def variar(self, valor:int):
+            if not isinstance (vlor, in)
+            
